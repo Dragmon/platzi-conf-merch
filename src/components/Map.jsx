2f0986e046a97b4c6f4fe75dfc9a35bfe342d3ab
Carlos Pérez <carlosperez@Mac-mini-de-Carlos.local>
2020-12-24 00:47:29 -0600

    integration google maps

---
+++ src/components/Map.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Map = ({data}) => {
+  const mapStyles = {
+    height: '50vh',
+    width: '100%',
+  }
+
+  const defaultCenter = {
+    lat:data.lat, lng: data.lng
+  }
+  return(
+    <LoadScript googleMapsApiKey={process.env.API_KEY_GOOGLE_MAPS}>
+      <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={defaultCenter}>
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default Map;
---
+++ src/hooks/useGoogleAddres.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useGoogleAddress = (address) => {
+  const [map, setMap] = useState({});
+  const API = `https://maps.googleapis.com/maps/api/geocode/xml?address=${address}&key=${process.env.API_KEY_GOOGLE_MAPS}`;
+
+  useEffect(async () => {
+    const response = await axios(API);
+    setMap(response.data.results[0].geometry.location);
+  }, []);
+  return map;
+};
+
+export default useGoogleAddress;
